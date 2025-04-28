a1e7686a37b9655b8af0bdbfa2b75a6b72a3dbdd
Pillz4Axel <147772516+AxelRoman0578@users.noreply.github.com>
2025-04-27 23:06:51 -0500

    Add files via upload

--- Solucion_Codigo/ProyectoTaller3/src/EjecutorTerreno.java
+++ Solucion_Codigo/ProyectoTaller3/src/EjecutorTerreno.java
@@ -1,65 +1,69 @@
 public class EjecutorTerreno {
+    public static Terreno terreno1 = new Terreno();
+    public static Terreno terreno2;
     
+    public static void main(String[] args) {
+        terreno1 = new Terreno(2, 3, 4);
+        terreno1.calcularArea();
+        terreno1.calcularCostoTerreno();
+        System.out.println(terreno1);
+        
+        terreno2 = new Terreno(3, 3, 4);
+        terreno2.calcularArea();
+        terreno2.calcularCostoTerreno();
+        System.out.println(terreno2);
+    }
 }
- 
-class Terreno {
+
+class Terreno{
     public double ancho;
     public double largo;
-    public double valorMetroCuadrado;
+    public double valorMetro;
     public double area;
-    public double costoTerreno;         
-
-    public Terreno() {
-    }
-    public Terreno(double ancho, double largo, double valorMetroCuadrado) {
+    public double costoTerreno;
+    public Terreno() { } 
+    public Terreno(double ancho, double largo, double valorMetro) {
         this.ancho = ancho;
         this.largo = largo;
-        this.valorMetroCuadrado = valorMetroCuadrado;
+        this.valorMetro = valorMetro; 
     }
-
-    public double getAncho() {
-        return ancho;
-    }
-
-    
-
-    public double getLargo() {
-        return largo;
-    }
-
-    public double getValorMetroCuadrado() {
-        return valorMetroCuadrado;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public double getCostoTerreno() {
-        return costoTerreno;
-    }
-
     public void setAncho(double ancho) {
-        this.ancho = ancho;
-    }
-
+        this.ancho = ancho;  
+    } 
     public void setLargo(double largo) {
-        this.largo = largo;
+        this.largo = largo; 
+    } 
+    public void setValorMetro(double valorMetro) {
+        this.valorMetro = valorMetro;  
+    } 
+    public double getAncho() {
+        return ancho;  
+    } 
+    public double getLargo() {
+        return largo;  
+    } 
+    public double getValorMetro() {
+        return valorMetro; 
+    } 
+    public double getArea() {
+        return area; 
+    } 
+    public double getCostoTerreno() {
+        return costoTerreno; 
     }
-
-    public void setCostoTerreno(double costoTerreno) {
-        this.costoTerreno = costoTerreno;
+    public void calcularArea(){
+        this.area = this.ancho * this.largo;
     }
-    
+    public void calcularCostoTerreno(){
+        this.costoTerreno = this.area * this.valorMetro;
+    }
     @Override
     public String toString() {
-        return "Terreno{" + "ancho=" + ancho + ", largo=" + largo + ", valorMetroCuadrado=" + valorMetroCuadrado + ", area=" + area + ", costoTerreno=" + costoTerreno + '}';
+        return String.format("Terreno {%s%.2f %s%.2f %s%.2f %s%.2f %s%.2f}",
+                                      "ancho = ", this.getAncho(),
+                                      ", largo = ", this.getLargo(),
+                                      ", valorMetro = ", this.getValorMetro(),
+                                      ", area = ", this.getArea(),
+                                      ", costoTerreno = ", this.getCostoTerreno());
     }
-    
-    
-    
-    
-    
 }
-
-
